8ce55afdd57ec0a08ab124575efdef2813c74581
medowaled <aboelnaga020@gmail.com>
2025-08-05 23:36:02 +0200

    شش

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,7 @@
-import { jwtVerify } from "jose";
-
-interface UserJwtPayload {
-  jti: string;
-  iat: number;
-  id: string;
-  role: string;
-  name: string;
-}
-
-export function getJwtSecretKey(): Uint8Array {
+export function getJwtSecretKey() {
   const secret = process.env.NEXT_JWT_SECRET;
-  if (!secret) {
-    console.warn("JWT_SECRET is not set in environment variables, using default secret");
-    return new Uint8Array(Buffer.from('your-secret-key', 'utf-8'));
+  if (!secret || secret.length < 10) {
+    throw new Error("JWT Secret is not set or too short!");
   }
-  return new Uint8Array(Buffer.from(secret, 'utf-8'));
-}
-
-export async function verifyAuth(token: string): Promise<UserJwtPayload> {
-  try {
-    const { payload } = await jwtVerify(token, getJwtSecretKey());
-    return payload as unknown as UserJwtPayload;
-  } catch (err) {
-    console.error("Token verification failed:", err); 
-    throw new Error("Your token has expired or is invalid.");
-  }
+  return new TextEncoder().encode(secret);
 }
